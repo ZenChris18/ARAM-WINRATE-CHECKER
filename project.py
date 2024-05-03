973c819f29e3f694049218ec053b277f39e50d1d
ZenChris18 <121231422+ZenChris18@users.noreply.github.com>
2024-05-03 19:51:14 +0800

    able to get puuid from link

--- project.py
+++ project.py
@@ -4,12 +4,20 @@ riot_api_key = "RGAPI-5c36f9ab-eaff-4e41-bc82-3bac7f9e6791"
 
 def main():
     username = get_username()
-    print(username)
+    response = get_puuid(username)
+    print(response)
 
 def get_username():
     game_name = input("Enter your username: ").replace(" ", "_")
     tag_line = input("Enter your tagline: ")
     return (f"https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}?api_key={riot_api_key}")
+
+
+def get_puuid(a):
+    code = requests.get(a)
+    puuid = code.json()["puuid"]
+    return puuid
+    
 
 if __name__ == "__main__":
     main()
