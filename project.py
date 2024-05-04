6327116bb10c19156e76ad74d1b4f30666314d75
ZenChris18 <121231422+ZenChris18@users.noreply.github.com>
2024-05-05 00:06:32 +0800

    able to get matches from user

--- project.py
+++ project.py
@@ -1,26 +1,33 @@
+import requests
 from dotenv import load_dotenv
 import os
-import requests
 
 load_dotenv()
 riot_api_key = os.environ.get("riot_api_key")
 
 def main():
-    username = get_username_from_user()
-    response = get_puuid(username)
-    print(response)
+    server, game_name, tag_line = get_username_from_user()
+    puuid = get_puuid(server, game_name, tag_line)
+    matches = get_matches(server, puuid)
+    print(matches)
 
 def get_username_from_user():
     game_name = input("Enter your username: ").replace(" ", "_")
     tag_line = input("Enter your tagline: ")
-    return (f"https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}?api_key={riot_api_key}")
+    server = input("Enter Server: ")
+    return server, game_name, tag_line
 
+def get_puuid(server, game_name, tag_line):
+    url = f"https://{server}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}?api_key={riot_api_key}"
+    response = requests.get(url)
+    response.raise_for_status()  # Raise exception for HTTP errors
+    return response.json()["puuid"]
 
-def get_puuid(a):
-    code = requests.get(a)
-    puuid = code.json()["puuid"]
-    return puuid
-    
+def get_matches(server, puuid):
+    url = f"https://sea.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?queue=450&start=0&count=100&api_key={riot_api_key}"
+    response = requests.get(url)
+    response.raise_for_status()  # Raise exception for HTTP errors
+    return response.json()
 
 if __name__ == "__main__":
     main()
