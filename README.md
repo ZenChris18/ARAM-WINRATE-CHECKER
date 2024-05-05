6374e343c418b417d2c1b6c22c9e61d63095acac
ZenChris18 <121231422+ZenChris18@users.noreply.github.com>
2024-05-05 17:31:04 +0800

    added test_project

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ Libraries Used
 1. dotenv - Used this library for security purposes. Need to hide Keys that i don't want to share to the public
 2. OS - in order to access the .env file and get the data.
 3. requests - to extract the dict in a json file. 
+
+
+
+
+# isinstance() is a built-in function used to check if an object is an instance of a specified class or its subclasses.
+# It returns True if the object is an instance of the class or a subclass of it, otherwise, it returns False.
+# Syntax: isinstance(object, class)
+#   - object: The object you want to check.
+#   - class: The class or type you want to check against.
+# Example: isinstance(5, int) will return True because 5 is an instance of the int class.
+# Example: isinstance("hello", str) will return True because "hello" is an instance of the str class.
---
+++ test_project.py
@@ -0,0 +1,48 @@
+from project import get_username_from_user, get_puuid, get_matches, get_match_info, game_stats, win_rate_checker
+
+
+def main():
+    test_get_username_from_user()
+    test_get_match_info()
+    test_game_stats()
+    test_get_puuid()
+    test_get_matches()
+    test_win_rate_checker()
+
+def test_get_username_from_user():
+    assert get_username_from_user() == ("Invalid server. Please try again.",)
+    assert get_username_from_user("end") == ("end", None, None, None)
+    assert get_username_from_user("yummy_tea", "owo", "PH2") == ("asia", "yummy_tea", "owo", "PH2")
+
+def test_get_puuid():
+    assert get_puuid("asia", "yummy_tea", "owo") == "l3wrf6KuxlXEhT5yZN_kJUDmUfrlfI7soEpuinkbyBn_tHnLb6vmywwTZxZM54J7BKMws1vWo01qVw"
+
+def test_get_matches():
+    server = "ph2"
+    puuid = "l3wrf6KuxlXEhT5yZN_kJUDmUfrlfI7soEpuinkbyBn_tHnLb6vmywwTZxZM54J7BKMws1vWo01qVw"
+    matches = get_matches(server, puuid)
+    assert isinstance(matches, list)
+    assert all(isinstance(match, str) for match in matches)
+
+def test_get_match_info():
+    match_id = "example_match_id"
+    server = "example_server"
+    returned_data = get_match_info(server, match_id)
+
+    # Assert that the function returned a dictionary
+    assert isinstance(returned_data, dict)
+
+
+def test_game_stats():
+    info = {"info": {"participants": [{"win": True, "puuid": "invalid_puuid"}]}}
+    puuid = "valid_puuid"
+    win_status = game_stats(info, puuid)
+    assert win_status is None
+
+def test_win_rate_checker():
+    win, lose = win_rate_checker(True)
+    assert win == 1
+    assert lose == 0
+
+if __name__ == "__main__":
+    main()
